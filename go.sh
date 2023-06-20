7a6fa622d966b36928b3a58e42e5a858e5b42ec0
Jindra Helcl <jindra.helcl@gmail.com>
2023-06-20 18:45:40 +0300

    reformat go

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 lng1=cs
 lng2=en
 
@@ -91,21 +93,17 @@ for bt_iteration in {1..bts}; do
 done
 
 for src in lng1 lng2; do
-  if [ src = lng1 ]; then
-    tgt=lng2
-  else
-    tgt=lng1
-  fi
+    if [ src = lng1 ] ; then tgt=lng2; else tgt=lng1; fi
 
-  for seed in 7 222 1337 2131; do
-    model_dir=model_${src}-${tgt}-${seed}
-    mkdir -p model_dir
+    for seed in 7 222 1337 2131; do
+	model_dir=model_${src}-${tgt}-${seed}
+	mkdir -p model_dir
 
-    marian -c config.yml \
-      --seed $seed \
-      --train-sets=data/train.{$src,$tgt}.txt \
-      --valid-sets=data/dev.{$src,$tgt}.txt \
-      --model $model_dir/model.npz \
-      --num_devices 8
-  done
+	marian -c config.yml \
+	       --seed $seed \
+	       --train-sets=data/train.{$src,$tgt}.txt \
+	       --valid-sets=data/dev.{$src,$tgt}.txt \
+	       --model $model_dir/model.npz \
+	       --num_devices 8
+    done
 done
