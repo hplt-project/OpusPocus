15cfcd0a6c604a0f20153005ad5df7d520bbbf4a
Graeme Nail <graemenail.work@gmail.com>
2023-06-20 16:04:05 +0000

    Add notes and some decontaminate

--- go.sh
+++ go.sh
@@ -2,27 +2,59 @@
 
 set -eo pipefail
 
+# Requirements:
+#  - We need to keep in TSV format for OpusTrainer
+
 lng1=cs
 lng2=en
+
+# Requires:
+# 1. Already have written filters for your downloaded datastes through opuscleaner
+# 2. Configurations for training, backtranslation, and decoding, as well as model arch
+# 3. Selected dev/test sets and evaluation metric/stopping criteria
+
+get_seeded_random() {
+  # source of repeatable randomness for shuf
+  seed="$1"
+  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
+    </dev/zero 2>/dev/null
+}
+
+# Devset and test set are given as an initial state
+# We either
+# 1. Use some already established dataset
+# 2. Extract dev/devtest _before_ cleaning, and tidy them into some "cleaner" dev/devtest
 
 # STEP 0 apply opuscleaner
 raw_data="data/raw"
 clean_dest="data/clean"
+mkdir -p ${clean_dest}
 for pipeline in ${raw_data}/*.filters.json; do
 
   prefix=$(basename $pipeline)
   prefix=${prefix/%.filters.json/}
 
   (opuscleaner-clean $pipeline --parallel 8) > \
-    >(
-      tee \
-        >(cut -f1 | pigz >${clean_dest}/${prefix}.$lng1.gz) \
-        >(cut -f2 | pigz >${clean_dest}/${prefix}.$lng2.gz) \
-        >/dev/null
-    ) \
-    2> >(tee ${clean_dest}/${prefix}.log >&2)
+    >(pigz >${clean_dest}/para/opuscleaner.${prefix}.$lng1-$lng2.tsv.gz) \
+    2> >(tee ${clean_dest}/para/${prefix}.$lng1-$lng2.log >&2)
+
+  # REMOVE DEV/TEST FROM THESE OUTPUTS
+  # Compute hashes of source and target of dev/test set.
+  # If _either_ matches the line in the training data matches, omit it
+  pigz -dc \
+    ${clean_dest}/opuscleaner.${prefix}.$lng1-$lng2.tsv.gz | decontaminate.py \
+    --to-remove \
+    data/dev/dev.${lng1}-${lng2}.tsv \
+    data/dev/devtest.${lng1}-${lng2}.tsv |
+    pigz ${clean_dest}/para/${prefix}.$lng1-$lng2.tsv.gz
 
 done
+
+# Get the prefixes for "clean" labelled datasets from opuscleaner
+clean_ds=$(python -c "import json; ds=json.load(open('${raw_data}/categories.json'))['mapping']['clean']; print(' '.join(ds))")
+for ds_prefix in clean_ds; do
+  cat ${clean_dest}/para/${ds_prefix}.$lng1-$lng2.tsv.gz
+done >data/train/clean.$lng1-$lng2.tsv.gz
 
 # STEP 1 generate vocab(s)
 PREFIX="--model_prefix=model.${lng1}-${lng2}"
@@ -34,7 +66,7 @@ spm_train \
   --unk_id=1 \
   ${PREFIX} \
   --vocab_size=${VOCAB_SIZE} \
-  --input=<(sed 's/\t/\n/g' <data/train-${lng1}-${lng2}.tsv) \
+  --input=<(sed 's/\t/\n/g' <data/train/clean.$lng1-$lng2.tsv.gz) \
   --input_sentence_size=20000000 \
   --train_extremely_large_corpus \
   --byte_fallback
@@ -53,6 +85,19 @@ spm_train \
 # 	   --num_devices 8
 # }
 
+# STEP 2.pre
+# When OpusCleaner supports mono-lingual, we should do cleaning here too
+# However, we also need to remove the dev/devtets from the monolingual data
+# Mono is cleaned+merged earlier
+for ln in $lng1 $lng2; do
+  pigz -dc \
+    data/raw/mono.$ln.txt.gz | decontaminate.py \
+    --to-remove \
+    data/dev/dev.${lng1}-${lng2}.tsv \
+    data/dev/devtest.${lng1}-${lng2}.tsv |
+    pigz ${clean_dest}/mono.$lng1-$lng2.tsv.gz
+done
+
 # STEP 2 train model in direction lng1 -> lng2
 bts=5
 for bt_iteration in {1..bts}; do
@@ -61,17 +106,17 @@ for bt_iteration in {1..bts}; do
   seed=1
 
   # train lng1 -> lng2
-  marian -c config.yml \
+  marian -c config-bt.yml \
     --seed $seed \
     --train-sets data/train-$((bt_iteration - 1)).{$src,$tgt}.txt \
-    --valid-sets data/dev.{$src,$tgt}.txt \
+    --valid-sets data/dev/dev.{$src,$tgt}.txt \
     --model model-${bt_iteration}/model.npz \
     --num_devices 8
 
   # translate lng1 (mono) -> lng2 (synth i-th iteration)
   marian-decoder \
     -m model-${bt_iteration}/model.npz \
-    -i data/mono.$src.txt \
+    -i data/clean/mono.$src.txt \
     -o data/synth-${bt_iteration}.$tgt.txt \
     --maxi-batch 100 --maxi-batch-sort src \
     --beam-size 6 \
@@ -80,9 +125,14 @@ for bt_iteration in {1..bts}; do
     --max-length 300 \
     --num-devices 8
 
+  # We must apply some light cleaning on the output of the
+  # iterative backtranslation to filter out length-ratios
+  bash clean-para.sh <(paste data/clean/mono.$src.txt synth-${bt_iteration}.$tgt.txt) \
+    >cleaned-synth-${bt_iteration}.tsv
+
   # prepare data (mix of auth + synth from i-th iteration)
   cat <(paste data/train.$src.txt data/train.$tgt.txt) \
-    <(paste mono.$src.txt synth-${bt_iteration}.$tgt.txt) |
+    cleaned-synth-${bt_iteration}.tsv |
     shuf |
     tee >(cut -f1 data/train-${bt_iteration}.$tgt.txt) \
       >(cut -f2 data/train-${bt_iteration}.$src.txt) \
@@ -93,17 +143,17 @@ for bt_iteration in {1..bts}; do
 done
 
 for src in lng1 lng2; do
-    if [ src = lng1 ] ; then tgt=lng2; else tgt=lng1; fi
+  if [ src = lng1 ]; then tgt=lng2; else tgt=lng1; fi
 
-    for seed in 7 222 1337 2131; do
-	model_dir=model_${src}-${tgt}-${seed}
-	mkdir -p model_dir
+  for seed in 7 222 1337 2131; do
+    model_dir=model_${src}-${tgt}-${seed}
+    mkdir -p model_dir
 
-	marian -c config.yml \
-	       --seed $seed \
-	       --train-sets=data/train.{$src,$tgt}.txt \
-	       --valid-sets=data/dev.{$src,$tgt}.txt \
-	       --model $model_dir/model.npz \
-	       --num_devices 8
-    done
+    marian -c config.yml \
+      --seed $seed \
+      --train-sets=data/train.{$src,$tgt}.txt \
+      --valid-sets=data/dev.{$src,$tgt}.txt \
+      --model $model_dir/model.npz \
+      --num_devices 8
+  done
 done
