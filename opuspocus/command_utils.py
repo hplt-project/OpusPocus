56adb8da50e78452151ea4e2660661c3c2041192
varisd <varis@ufal.mff.cuni.cz>
2023-12-18 15:33:49 +0200

    command_utils.py: fixed process selection bug

--- opuspocus/command_utils.py
+++ opuspocus/command_utils.py
@@ -10,6 +10,8 @@ logger = logging.getLogger(__name__)
 def build_subprocess(cmd_path, args, jid_deps):
     if args.runner == 'sbatch':
         cmd = build_sbatch(cmd_path, args, jid_deps)
+    elif args.runner == 'sbatch-hq':
+        cmd = build_sbatch_hq(cmd_path, args, jid_deps)
     else:
         raise NotImplementedError()
     sub = {
@@ -18,7 +20,7 @@ def build_subprocess(cmd_path, args, jid_deps):
         ),
         'jobid' : None,
     }
-    if args.runner == 'sbatch':
+    if args.runner == 'sbatch' or args.runner == 'sbatch-hq':
         sub['jobid'] = int(sub['process'].stdout.readline())
 
     return sub
@@ -26,6 +28,23 @@ def build_subprocess(cmd_path, args, jid_deps):
 
 def build_sbatch(cmd_path, args, jid_deps):
     cmd = ['sbatch', '--parsable']
+
+    if jid_deps:
+        cmd.append('--dependency')
+        cmd.append(','.join(['afterok:{}'.format(jid) for jid in jid_deps]))
+
+    runner_opts = args.runner_opts
+    if runner_opts is not None:
+        cmd += runner_opts.split(' ')
+
+    cmd.append(str(cmd_path))
+    logger.info('Executing command: "{}"'.format(' '.join(cmd)))
+
+    return cmd
+
+def build_sbatch_hq(cmd_path, args, jid_deps):
+    """Sbatch hyperqueue support for LUMI HPC."""
+    cmd = ['sbatch_hq', '--parsable']
 
     if jid_deps:
         cmd.append('--dependency')
