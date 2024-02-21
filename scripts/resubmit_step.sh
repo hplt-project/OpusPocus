530c4fa6523780d3fcf141babecbeafd5f3ebfe5
varisd <varis@ufal.mff.cuni.cz>
2024-02-21 16:53:24 +0200

    bug fixing in resubmit step

--- scripts/resubmit_step.sh
+++ scripts/resubmit_step.sh
@@ -9,11 +9,13 @@ SLURM_JOBID=$(cat $STEP_DIR/step.jobid)
 
 new_jid=$( sbatch --parsable $SLURM_PARAMS $STEP_DIR/step.command )
 
+#rm -r $STEP_DIR/logs/*
+
 echo $new_jid > $STEP_DIR/step.jobid
 echo RUNNING > $STEP_DIR/step.state
 
 # Update the dependent steps
-for job in $(sqeueu --me --format "%i %E" | grep ":$SLURM_JOBID" | grep -v ^$new_jid | cut -d" " -f1); do
+for job in $(squeue --me --format "%i %E" | grep ":$SLURM_JOBID" | grep -v ^$new_jid | cut -d" " -f1); do
     echo Updating dependencies of job $job... >&2
     update_str=$(squeue --me --format "%i %E" \
         | grep ^$job \
