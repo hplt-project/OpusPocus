1c510703813c6e9bc1c0ebe4893600c92393620e
varisd <varis@ufal.mff.cuni.cz>
2024-06-07 11:40:04 +0200

    update to pipeline configs and examples

--- examples/train_only.backtranslation.init_and_run.sh
+++ examples/train_only.backtranslation.init_and_run.sh
@@ -2,10 +2,10 @@
 
 SRC=en
 TGT=nn
-EXP_DIR="experiments/$SRC-$TGT/preprocess_only"
+EXP_DIR="experiments/$SRC-$TGT/train_only.backtranslation"
 
 echo Initializing pipeline... >&2
-./go.py init --pipeline-config config/pipeline.preprocess.yml --pipeline-dir $EXP_DIR
+./go.py init --pipeline-config config/pipeline.train.backtranslation.yml --pipeline-dir $EXP_DIR
 echo >&2
 
 echo Running pipeline... >&2
