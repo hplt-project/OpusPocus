77e28c510037e69621e8448fb9e13b24509e1afd
varisd <varis@ufal.mff.cuni.cz>
2024-07-30 18:04:27 +0200

    EvaluateStep unit tests

---
+++ tests/opuspocus/pipeline_steps/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+from opuspocus.pipeline_steps.raw import RawCorpusStep
+
+
+@pytest.fixture(scope="module")
+def raw_step_train_minimal(pipeline_dir, data_train_minimal):
+    src_lang = data_train_minimal[0].suffix
+    tgt_lang = data_train_minimal[1].suffix
+    step = RawCorpusStep(
+        step="raw",
+        step_label="raw.{}-{}".format(src_lang, tgt_lang),
+        pipeline_dir=pipeline_dir,
+        raw_data_dir=data_train_minimal[0].parent,
+        src_lang=src_lang,
+        tgt_lang=tgt_lang,
+    )
+    step.init_step()
+    return step
+
+
+@pytest.fixture(scope="module")
+def raw_step_train_minimal_reversed(pipelin_dir, data_train_minimal):
+    src_lang = data_train_minimal[1].suffix
+    tgt_lang = data_train_minimal[0].suffix
+    step = RawCorpusStep(
+        step="raw",
+        step_label="raw.{}-{}".format(src_lang, tgt_lang),
+        pipeline_dir=pipeline_dir,
+        raw_data_dir=data_train_minimal[0].parent,
+        src_lang=src_lang,
+        tgt_lang=tgt_lang
+    )
+    step.init_step()
+    return step
