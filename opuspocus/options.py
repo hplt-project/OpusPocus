2822537483119d14e9507969e097133f10d10c44
varisd <varis@ufal.mff.cuni.cz>
2024-10-24 10:04:48 +0200

    CLI update: updated run, stop subcommands

--- opuspocus/options.py
+++ opuspocus/options.py
@@ -54,7 +54,10 @@ def parse_run_args(argv: Sequence[str]) -> argparse.Namespace:
 
     _add_general_arguments(parser, pipeline_dir_required=False)
     parser.add_argument("--reinit", default=False, action="store_true", help="Re-initialize an existing pipeline.")
-    parser.add_argument("--rerun-completed", default=False, action="store_true", help="Re-run finished steps.")
+    parser.add_argument(
+        "--stop-previous-run", default=False, action="store_true", help="Stop previous pipeline execution first."
+    )
+    parser.add_argument("--resubmit-done", default=False, action="store_true", help="Re-run finished steps.")
     parser.add_argument("--pipeline-config", type=file_path, default=None, help="Pipeline configuration YAML file.")
     parser.add_argument(
         "--runner",
@@ -82,15 +85,6 @@ def parse_stop_args(argv: Sequence[str]) -> argparse.Namespace:
     parser = OpusPocusParser(description=f"{GENERAL_DESCRIPTION}: Pipeline Termination")
 
     _add_general_arguments(parser, pipeline_dir_required=True)
-    # TODO(varisd): Can we make so that the correct runner loads from the runner.* files?
-    parser.add_argument(
-        "--runner",
-        type=str,
-        metavar="RUNNER",
-        required=True,
-        choices=RUNNER_REGISTRY.keys(),
-        help="Runner used for pipeline execution manipulation.",
-    )
     parser.add_argument(
         "--targets",
         type=str,
--- opuspocus_cli/run.py
+++ opuspocus_cli/run.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 import logging
 import sys
+import warnings
 from argparse import Namespace
 from pathlib import Path
 from typing import Sequence
 
 from opuspocus.options import parse_run_args
 from opuspocus.pipelines import OpusPocusPipeline, PipelineConfig, PipelineState, build_pipeline, load_pipeline
-from opuspocus.runners import build_runner
+from opuspocus.runners import build_runner, load_runner
 
 logger = logging.getLogger(__name__)
 
 
 def parse_args(argv: Sequence[str]) -> Namespace:
     return parse_run_args(argv)
+
+
+def init_pipeline(pipeline: OpusPocusPipeline, args: Namespace) -> OpusPocusPipeline:
+    if pipeline is None:
+        pipeline = build_pipeline(args)
+        pipeline.init()
+    elif pipeline.state == PipelineState.INIT_INCOMPLETE:
+        logger.info("An existing pipeline's initialization is incomplete. Finishing initialization...")
+        pipeline.init()
+    elif args.reinit:
+        logger.info("Re-initializing the pipeline...")
+        if pipeline.state in [PipelineState.RUNNING, PipelineState.SUBMITTED]:
+            logger.info("Stopping the previous run...")
+            prev_runner = load_runner(pipeline.pipeline_dir)
+            prev_runner.stop_pipeline(pipeline)
+        pipeline.reinit()
+    return pipeline
 
 
 def main(args: Namespace) -> int:
@@ -33,39 +51,61 @@ def main(args: Namespace) -> int:
 
     # As a fallback, we can re-use config from the provided --pipeline-dir
     if args.pipeline_config is None:
-        setattr(args, Path(args.pipeline_dir, OpusPocusPipeline.config_file))
+        args.pipeline_config = Path(args.pipeline_dir, OpusPocusPipeline.config_file)
         logger.info("No --pipeline-config was provided, reading pipeline configuration from %s", args.pipeline_config)
 
     # By default, we use the pipeline directory defined in the config file
     if args.pipeline_dir is None:
         config_file = PipelineConfig.load(args.pipeline_config)
-        setattr(args, config_file["pipeline"]["pipeline_dir"])
+        args.pipeline_dir = Path(config_file["pipeline"]["pipeline_dir"])
 
     # First, we try to load a pipeline if it was previously saved
     try:
         pipeline = load_pipeline(args)
-        logger.info("An existing pipeline located at %s was loaded.")
+        logger.info("An existing pipeline located at %s was loaded.", pipeline.pipeline_dir)
         # TODO(varisd): existing pipeline overwrite logic
     except Exception:  # noqa: BLE001
         pipeline = None
 
-    # Initialization phase
-    if pipeline is None:
-        pipeline = build_pipeline(args)
-        pipeline.init()
-    elif pipeline.state == PipelineState.INIT_INCOMPLETE:
-        logger.info("An existing pipeline's initialization is incomplete. Finishing initialization...")
-        pipeline.init()
-    elif args.reinit:
-        if pipeline.state == PipelineState.RUNNING:
-            logger.info("Re-initializing running pipeline. Stopping the previous run...")
-            # TODO(varisd): add necessary logic (stopping pipeline without an explicit runner)
-            raise NotImplementedError()
-        pipeline.reinit()
+    ## Initialization phase ##
+    pipeline = init_pipeline(pipeline, args)
 
-    # Running phase
-    runner = build_runner(args.runner, pipeline.pipeline_dir, args)
-    runner.run_pipeline(pipeline, targets=pipeline.get_targets(args.targets), rerun_completed=args.rerun_completed)
+    # The pipeline must be at least INITED at this point
+    if pipeline.state is None or pipeline.state == PipelineState.INIT_INCOMPLETE:
+        logger.error("Something went wrong with pipeline initialization (Current pipeline state: %s", pipeline.state)
+        sys.exit(2)
+
+    ## Run phase ##
+    runner = build_runner(args.runner, args.pipeline_dir, args)
+
+    # Check the pipeline state and whether it requires --reinit or --rerun flags
+    if args.stop_previous_run:
+        stopped_states = [
+            PipelineState.INIT_INCOMPLETE,
+            PipelineState.INITED,
+            PipelineState.DONE,
+            PipelineState.FAILED,
+            None,
+        ]
+        if pipeline.state in stopped_states:
+            warnings.warn(
+                f"Pipeline in the {pipeline.state} does not need to be stopped. "
+                "Ignoring the --stop-previous-run option.",
+                UserWarning,
+                stacklevel=1,
+            )
+        else:
+            prev_runner = load_runner(pipeline.pipeline_dir)
+            prev_runner.stop_pipeline(pipeline)
+    elif pipeline.state in [PipelineState.SUBMITTED, PipelineState.RUNNING]:
+        logger.error(
+            "You are trying to run pipeline in the %s state. Use --stop-previous-run option to first stop "
+            "the previous execution.",
+            pipeline.state,
+        )
+        sys.exit(2)
+
+    runner.run_pipeline(pipeline, targets=pipeline.get_targets(args.targets), resubmit_done=args.resubmit_done)
     return 0
 
 
